708bb33d1659af8fd8f4307ee11a258bbca85a45
fuziki <hymne.shannon.projects@gmail.com>
2019-09-18 00:33:11 +0900

    add example for mac

---
+++ ExampleMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ExampleMacOS
+//
+//  Created by fuziki on 2019/09/17.
+//  Copyright © 2019 fuziki.factory. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ExampleMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ExampleMacOS
+//
+//  Created by fuziki on 2019/09/17.
+//  Copyright © 2019 fuziki.factory. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
