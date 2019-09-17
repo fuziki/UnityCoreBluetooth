5fa064b61f8970204f685e61f820ae3607cbb9b3
fuziki <hymne.shannon.projects@gmail.com>
2019-09-18 00:32:43 +0900

    add framewrok for ios target

---
+++ UnityCoreBluetoothIOS/UnityCoreBluetoothIOS.h
@@ -0,0 +1,19 @@
+//
+//  UnityCoreBluetoothIOS.h
+//  UnityCoreBluetoothIOS
+//
+//  Created by fuziki on 2019/09/17.
+//  Copyright © 2019 fuziki.factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UnityCoreBluetoothIOS.
+FOUNDATION_EXPORT double UnityCoreBluetoothIOSVersionNumber;
+
+//! Project version string for UnityCoreBluetoothIOS.
+FOUNDATION_EXPORT const unsigned char UnityCoreBluetoothIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnityCoreBluetoothIOS/PublicHeader.h>
+
+
