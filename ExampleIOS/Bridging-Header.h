77cd643e2cdcde0dfddf825c84b34e084e8ef424
fuziki <hymne.shannon.projects@gmail.com>
2019-09-18 00:31:38 +0900

    add bundle for mac target

---
+++ ExampleIOS/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  ExampleIOS
+//
+//  Created by fuziki on 2019/09/17.
+//  Copyright © 2019 fuziki.factory. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+//#import "../UnityCoreBluetooth/UnityCoreBluetooth.mm"
+
+#endif /* Bridging_Header_h */
---
+++ ExampleIOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ExampleIOS
+//
+//  Created by fuziki on 2019/09/17.
+//  Copyright © 2019 fuziki.factory. All rights reserved.
+//
+
+import UIKit
+import UnityCoreBluetoothIOS
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+//        let a = unityCoreBluetooth_init()
+        
+    }
+
+
+}
+
