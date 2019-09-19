917e185229d448fc866f1615625926d8ab894bb9
fuziki <hymne.shannon.projects@gmail.com>
2019-09-19 23:04:43 +0900

    add static ios library

---
+++ libUnityCoreBluetoothIOS/libUnityCoreBluetoothIOS.swift
@@ -0,0 +1,11 @@
+//
+//  libUnityCoreBluetoothIOS.swift
+//  libUnityCoreBluetoothIOS
+//
+//  Created by fuziki on 2019/09/19.
+//  Copyright © 2019 fuziki.factory. All rights reserved.
+//
+
+class libUnityCoreBluetoothIOS {
+
+}
