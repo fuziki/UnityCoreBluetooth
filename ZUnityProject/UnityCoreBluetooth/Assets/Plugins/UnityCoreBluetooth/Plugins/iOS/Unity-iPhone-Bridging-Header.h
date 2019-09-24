bc32be97918b5c153eb61c242f4f64652f644d14
fuziki <hymne.shannon.projects@gmail.com>
2019-09-24 13:53:31 +0900

    support ios

---
+++ ZUnityProject/UnityCoreBluetooth/Assets/Plugins/UnityCoreBluetooth/Plugins/iOS/Unity-iPhone-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Unity-iPhone-Bridging-Header.h
+//  Unity-iPhone
+//
+//  Created by fuziki on 2019/09/24.
+//
+
+#ifndef Unity_iPhone_Bridging_Header_h
+#define Unity_iPhone_Bridging_Header_h
+
+
+#endif /* Unity_iPhone_Bridging_Header_h */
