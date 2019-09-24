6887ec852589a45e098e6d2bf81cb6282c5fd10a
fuziki <hymne.shannon.projects@gmail.com>
2019-09-24 13:53:31 +0900

    support ios

--- ZUnityProject/UnityCoreBluetooth/Assets/Plugins/UnityCoreBluetooth/Scripts/UnityCBCharacteristic.cs
+++ ZUnityProject/UnityCoreBluetooth/Assets/Plugins/UnityCoreBluetooth/Scripts/UnityCBCharacteristic.cs
@@ -1,47 +1,50 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-public class UnityCBCharacteristic {
-
-    [DllImport("UnityCoreBluetoothMacOS")]
-    private static extern string cbCharacteristic_uuid(IntPtr characteristic);
-
-    [DllImport("UnityCoreBluetoothMacOS")]
-    private static extern string cbCharacteristic_propertyString(IntPtr characteristic);
-
-    [DllImport("UnityCoreBluetoothMacOS")]
-    private static extern void cbCharacteristic_setNotifyValue(IntPtr characteristic, bool enable);
-
-
-    private IntPtr nativePtr;
-    public UnityCBCharacteristic(IntPtr ptr)
+#if UNITY_EDITOR_OSX || UNITY_IOS
+namespace UnityCoreBluetoothFramework
+{
+    public class UnityCBCharacteristic
     {
-        this.nativePtr = ptr;
-    }
+        [DllImport(LIBRARY.NAME)]
+        private static extern string cbCharacteristic_uuid(IntPtr characteristic);
 
-    private string _uuid = null;
-    public string uuid
-    {
-        get
+        [DllImport(LIBRARY.NAME)]
+        private static extern string cbCharacteristic_propertyString(IntPtr characteristic);
+
+        [DllImport(LIBRARY.NAME)]
+        private static extern void cbCharacteristic_setNotifyValue(IntPtr characteristic, bool enable);
+
+        private IntPtr nativePtr;
+        public UnityCBCharacteristic(IntPtr ptr)
         {
-            if (_uuid == null) _uuid = cbCharacteristic_uuid(nativePtr);
-            return _uuid;
+            this.nativePtr = ptr;
+        }
+
+        private string _uuid = null;
+        public string uuid
+        {
+            get
+            {
+                if (_uuid == null) _uuid = cbCharacteristic_uuid(nativePtr);
+                return _uuid;
+            }
+        }
+
+        private string[] _propertis = null;
+        public string[] propertis
+        {
+            get
+            {
+                if (_propertis == null) _propertis = new string[] { cbCharacteristic_propertyString(nativePtr) };
+                return _propertis;
+            }
+        }
+
+        public void setNotifyValue(bool enable)
+        {
+            cbCharacteristic_setNotifyValue(nativePtr, enable);
         }
     }
-
-    private string[] _propertis = null;
-    public string[] propertis
-    {
-        get
-        {
-            if (_propertis == null) _propertis = new string[] { cbCharacteristic_propertyString(nativePtr) };
-            return _propertis;
-        }
-    }
-
-    public void setNotifyValue(bool enable)
-    {
-        cbCharacteristic_setNotifyValue(nativePtr, enable);
-    }
-
 }
+#endif
